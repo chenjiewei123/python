4ef2a977880ea3b211930c6d8c733de6f1035580
e <2212659654@qq.com>
2019-11-15 13:36:59 +0800

    01-test.py

---
+++ 01-test.py
@@ -0,0 +1,19 @@
+#定义函数：
+def print_menu():
+	print("=="*50)
+	print("名片管理系统")
+	print("1xxx")
+	print("2xxx")
+	print("=="*50)
+#定义三角形
+def print_sanjiaoxing():
+	print("*")
+	print("*")
+	print("*")
+	print("*")
+	print("*")
+
+#调用函数：
+print_menu()
+
+print_sanjiaoxing()
